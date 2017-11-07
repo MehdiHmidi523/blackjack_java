176623d161ad809fd6095930ae8aee6436ca221b
Mehdi Hmidi <mh223vk@student.lnu.se>
2017-11-07 11:42:09 +0100

    Attempt Fix starting point

--- BlackJack/controller/PlayGame.java
+++ BlackJack/controller/PlayGame.java
@@ -8,40 +8,23 @@ public class PlayGame implements IObserver{
 
     private Game game;
     private IView view;
+
     public PlayGame(Game a_game, IView a_view){
         game = a_game;
         view = a_view;
         a_game.register(this);
     }
 
-    public boolean Play(Game a_game, IView a_view) {
-
-        a_view.DisplayClearConsole();
-        a_view.DisplayWelcomeMessage();
-
-        view.DisplayDealerHand(game.GetDealerHand(), game.GetDealerScore());
-        view.DisplayPlayerHand(game.GetPlayerHand(), game.GetPlayerScore());
-
-        if (a_game.IsGameOver()) {
-            a_view.DisplayGameOver(a_game.IsDealerWinner());
-        }
-        IView.InputChoice choice = a_view.GetInput();
-        switch (choice) {
-            case P:
-                a_game.NewGame();
-                break;
-            case H:
-                a_game.Hit();
-                view.pauseProgram();
-                break;
-            case S:
-                a_game.Stand();
-                view.pauseProgram();
-                break;
-        }
-        return choice != IView.InputChoice.Q;
+    public void Play() {
+        view.DisplayClearConsole();
+        view.DisplayWelcomeMessage();
+        showPlayerHands();
     }
 
+    public void showPlayerHands(){
+        view.DisplayDealerHand(game.GetDealerHand(), game.GetDealerScore());
+        view.DisplayPlayerHand(game.GetPlayerHand(), game.GetPlayerScore());
+    }
     public void update() {
         view.DisplayDealerHand(game.GetDealerHand(), game.GetDealerScore());
         view.DisplayPlayerHand(game.GetPlayerHand(), game.GetPlayerScore());
--- BlackJack/view/SimpleView.java
+++ BlackJack/view/SimpleView.java
@@ -3,66 +3,42 @@ package BlackJack.view;
 import java.util.Scanner;
 
 public class SimpleView implements IView {
-  public void DisplayWelcomeMessage()
-        {
-          for(int i = 0; i < 2; i++) {System.out.print("\n");};
-            System.out.println(" _____________________________________________");
-            System.out.println("|                                             |");
-            System.out.println("| ҉҉҉҉҉҉     Welcome to BLACKJACK      ҉҉҉҉҉҉ |") ;
-            System.out.println("|_____________________________________________|\n");
+  public void DisplayWelcomeMessage() {
+      for(int i = 0; i < 2; i++) {System.out.print("\n");};
+      System.out.println(" _____________________________________________");
+      System.out.println("|                                             |");
+      System.out.println("| ҉҉҉҉҉҉     Welcome to BLACKJACK      ҉҉҉҉҉҉ |") ;
+      System.out.println("|_____________________________________________|\n");
+      System.out.println("Type 'p' to Play, 'h' to Hit, 's' to Stand or 'q' to Quit\n");
+  }
 
-            System.out.println("Type 'p' to Play, 'h' to Hit, 's' to Stand or 'q' to Quit\n");
-        }
+    public void DisplayCard(BlackJack.model.Card a_card) {
+      System.out.println("" + a_card.GetValue() + " of " + a_card.GetColor());
+    }
 
-        public InputChoice GetInput() {
-            Scanner sc = new Scanner(System.in);
-          while (true) {
-              try {
-                  String in = sc.nextLine();
-                  switch (in) {
-                      case "p": return InputChoice.P;
-                      case "h": return InputChoice.H;
-                      case "s": return InputChoice.S;
-                      case "q": return InputChoice.Q;
-                  }
-              }
-              catch (IllegalArgumentException e) {
-                  System.out.println("Not a valid input, please try again.");
-              }
-          }
-        }
+    public void DisplayPlayerHand(Iterable<BlackJack.model.Card> a_hand, int a_score) {
+      DisplayHand("Player", a_hand, a_score);
+    }
 
-        public void DisplayCard(BlackJack.model.Card a_card) {
-            System.out.println("" + a_card.GetValue() + " of " + a_card.GetColor());
-        }
+    public void DisplayDealerHand(Iterable<BlackJack.model.Card> a_hand, int a_score) {
+        DisplayHand("Dealer", a_hand, a_score);
+    }
 
-        public void DisplayPlayerHand(Iterable<BlackJack.model.Card> a_hand, int a_score) {
-            DisplayHand("Player", a_hand, a_score);
+    private void DisplayHand(String a_name, Iterable<BlackJack.model.Card> a_hand, int a_score) {
+        System.out.println(a_name + " Has: ");
+        for(BlackJack.model.Card c : a_hand) {
+            DisplayCard(c);
         }
+        System.out.println("Score: " + a_score);
+        System.out.println("");
+    }
 
-        public void DisplayDealerHand(Iterable<BlackJack.model.Card> a_hand, int a_score) {
-            DisplayHand("Dealer", a_hand, a_score);
-        }
+    public void DisplayGameOver(boolean a_dealerIsWinner) {
+        System.out.println("GameOver: ");
+        if (a_dealerIsWinner)  System.out.println("Dealer Won!");
+        else  System.out.println("You Won!");
+    }
 
-        private void DisplayHand(String a_name, Iterable<BlackJack.model.Card> a_hand, int a_score) {
-            System.out.println(a_name + " Has: ");
-            for(BlackJack.model.Card c : a_hand) {
-                DisplayCard(c);
-            }
-            System.out.println("Score: " + a_score);
-            System.out.println("");
-        }
-
-        public void DisplayGameOver(boolean a_dealerIsWinner) {
-            System.out.println("GameOver: ");
-            if (a_dealerIsWinner) {
-                System.out.println("Dealer Won!");
-            }
-            else {
-                System.out.println("You Won!");
-            }
-            
-        }
     public void pauseProgram() {
         try {
             Thread.sleep(2500);
@@ -70,9 +46,21 @@ public class SimpleView implements IView {
             e.printStackTrace();
         }
     }
-    public void DisplayClearConsole(){
-        System.out.flush();
+
+    public void DisplayClearConsole(){ System.out.flush(); }
+
+    @Override
+    public void visit(AmericanNewGameStrategy americanNewGameStrategy) {
+        System.out.println("Game Strategy: American");
     }
-
-
+    @Override
+    public void visit(InternationalNewGameStrategy internationalNewGameStrategy) { System.out.println("Game Strategy: International"); }
+    @Override
+    public void visit(BasicHitStrategy basicHitStrategy) { System.out.println("Hit Strategy: Basic"); }
+    @Override
+    public void visit(Soft17HitStrategy soft17HitStrategy) { System.out.println("Hit Strategy: Soft17"); }
+    @Override
+    public void visit(D_WinningRule d_winingRule) { System.out.println("Winning Rule: Dealer"); }
+    @Override
+    public void visit(P_WinningRule p_WiningRule) { System.out.println("Winning Rule: Player"); }
 }
